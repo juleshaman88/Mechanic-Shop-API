e33fe56e5977bc5199a880891e1f8976ad4e31b8
Julianna Haman <julianna.haman@gmail.com>
2026-07-05 13:34:10 -0700

    Updated config andn app.py settings.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from app import create_app
 from app.extensions import db
+from flask import Flask
 
-app = create_app()
+app = create_app('DevelopmentConfig')
 
 with app.app_context():
     db.create_all()
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5500)
+    app.run()
---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+import urllib.parse
+
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = urllib.parse.quote_plus(os.getenv("DB_PASSWORD", "Ravioli@2012?!?!"))
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME", "mechanic_api")
+
+
+class BaseConfig:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
